816417ff6162ece76b6e7a084cff00e3c1c2f481
flan <flan@arch.fyi>
2023-12-02 21:26:59 -0500

    ii

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import HelloWorld from './HelloWorld';
+import Counter from './Counter';
+import FilteredList from './FilteredList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//TODO (FilteredList): create a produce list using the 'const' variable keyword
+const produce =
+[
+{name: "Apple", type: "Fruit"},
+{name: "Pineapple", type: "Fruit"} ,
+{name: "Banana", type: "Fruit"},
+{name: "Pear", type: "Fruit"},
+{name: "Strawberry", type: "Fruit"},
+{name: "Orange", type: "Fruit"},
+{name: "Lettuce", type: "Vegetable"},
+{name: "Cucumber", type: "Vegetable"},
+{name: "Eggplant", type: "Vegetable"},
+{name: "Squash", type: "Vegetable"},
+{name: "Bell pepper", type: "Vegetable"},
+{name: "Onion", type: "Vegetable"},
+];
+
+class App extends Component {
+    render() {
+        return (
+
+			<div className="App">
+
+
+                /*TODO (HelloWorld): create an instance of the HelloWorld Component*/
+                <HelloWorld name={'[Holden]'}/>
+
+                /*TODO (Counter): create an instance of the Counter Component*/
+                <Counter/>
+
+            	/*TODO (FilteredList): create an instance of the FilteredList Component with a variable named items that takes in your produce list*/
+            <FilteredList items={produce}/>
+            </div>
+        );
+    }
 }
 
 export default App;
